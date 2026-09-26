8370276ff6ab7437b0f79740f91756bfbc131b34
phosphor-talons <P30.974@proton.me>
2026-09-25 20:53:10 -0400

    Add files via upload
    now with dirty hack :D

--- packaging/install.sh
+++ packaging/install.sh
@@ -114,7 +114,10 @@ build_candidates() {
                     add_candidate "Orca Slicer (Snap)" \
                         "OrcaSlicer.conf" "network_plugin_version" \
                         "${HOME}/snap/orcaslicer/10/.config/OrcaSlicer"
-                    ;;
+		 add_candidate "Orca Slicer (Snap)" \
+                        "OrcaSlicer.conf" "network_plugin_version" \
+                        "${HOME}/snap/orcaslicer/9/.config/OrcaSlicer"
+		    ;;
                 Darwin)
                     add_candidate "Orca Slicer" \
                         "OrcaSlicer.conf" "network_plugin_version" \
